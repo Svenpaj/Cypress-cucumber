85141e6adeb563afbf0a106ece175880dfd6a0df
Svenpaj <114157351+Svenpaj@users.noreply.github.com>
2023-04-19 09:43:03 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 5500
+// const path = require('path');
+
+app.use(express.static('lumia-shop'))
+
+app.listen(port, () => {
+  console.log(`Test app listening on port ${port}`)
+})
